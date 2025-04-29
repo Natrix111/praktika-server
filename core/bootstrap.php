2ee255f049a7fc3b51780b3fbf7ae1ff5e83162e
Natrix111 <dmitryk414@gmail.com>
2025-04-29 13:05:47 +0700

    fix

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,10 +1,8 @@
 <?php
 //Путь до директории с конфигурационными файлами
 const DIR_CONFIG = '/../config';
-
 //Подключение автозагрузчика composer
 require_once __DIR__ . '/../vendor/autoload.php';
-
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
 {
@@ -17,7 +15,5 @@ function getConfigs(string $path = DIR_CONFIG): array
     }
     return $settings;
 }
-
 require_once __DIR__ . '/../routes/web.php';
-
 return new Src\Application(new Src\Settings(getConfigs()));
